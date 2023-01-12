bde5b7f662bff03a37917f7dd5280cc52603ddfa
Ahnaf Tazwar <ahnaftazwar368@gmail.com>
2023-01-12 18:53:07 +1100

    attempt at refactoring code
    non functional as redux is NECESSARy for maintaining states properly. maybe in the future lmao

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,13 @@ import { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
 import { pathResolvePlugin } from './plugins/path-resolution-plugin';
 import { moduleRequestPlugin } from './plugins/module-request-plugin';
-import MonacoEditor, { OnChange } from '@monaco-editor/react';
-import baseHTML from './components/base-output';
+import MonacoEditor, { OnChange, OnMount } from '@monaco-editor/react';
+import OutputWindow from './components/output-window';
+import 'bulmaswatch/cyborg/bulmaswatch.min.css';
 
 const App = () => {
-  const iframe = useRef<HTMLIFrameElement>(null);
   const [mainBuildService, setBuildService] = useState<esbuild.Service>();
+  const [code, setCode] = useState('');
   const [input, setInput] = useState('');
 
   // Prepares app for processing transpile requests
@@ -33,18 +34,17 @@ const App = () => {
     }
     setInput(value);
   };
+  const handleMount: OnMount = (monaco) => {
+    monaco.getModel()?.updateOptions({ tabSize: 2 });
+  };
 
   // Assigned to button to send a service req to esbuild
   const initiateTranspiling = async () => {
-    if (!mainBuildService || !iframe.current || !iframe.current.contentWindow) {
+    if (!mainBuildService) {
       return;
     }
 
-    // reset the iframe window before each code run to remove any changes to page
-    iframe.current.srcdoc = baseHTML;
-
-    // begin building file to pull in any imported libraries
-    // using the unpkg path plugin instead of the default
+    // begin building file with all modules using custom plugins
     const result = await mainBuildService.build({
       entryPoints: ['index.js'],
       bundle: true,
@@ -54,27 +54,36 @@ const App = () => {
       define: { 'process.env.NODE_ENV': '"production"' },
     });
 
-    // communicating all code through messages to maintain no relation between parent child
-    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
+    setCode(result.outputFiles[0].text);
   };
 
   return (
-    <div style={{display: "flex"}}>
-      <MonacoEditor
-        height='300px'
-        defaultLanguage='javascript'
-        defaultValue='/* Use "print()" to show items in the result widow */'
-        onChange={handleEditorChange}
-      />
-      <div>
-        <button onClick={initiateTranspiling}>Transpile!</button>
+    <div className='columns'>
+      <div className='column'>
+        <h1 className='title is-3'>Online Js editor</h1>
+        <MonacoEditor
+          theme='vs-dark'
+          height='300px'
+          defaultLanguage='javascript'
+          defaultValue='/* Use "print()" to show items in the result widow */'
+          options={{
+            wordWrap: 'on',
+            minimap: { enabled: false },
+            lineNumbersMinChars: 3,
+            folding: false,
+            fontSize: 16,
+            automaticLayout: true,
+          }}
+          onChange={handleEditorChange}
+          onMount={handleMount}
+        />
+        <button className='button is-primary' onClick={initiateTranspiling}>
+          Transpile!
+        </button>
       </div>
-      <iframe
-        ref={iframe}
-        title='code-result'
-        sandbox='allow-scripts'
-        srcDoc={baseHTML}
-      />
+      <div className='column box'>
+        <OutputWindow transpiledCode={code} />
+      </div>
     </div>
   );
 };
