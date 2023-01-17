d833c14a583f0145ad1738fc4b457f74837a461c
Ahnaf Tazwar <ahnaftazwar368@gmail.com>
2023-01-17 17:36:07 +1100

    minor fix for editor area expansion limit

--- src/components/Box.tsx
+++ src/components/Box.tsx
@@ -11,7 +11,7 @@ const Box = ({ direction, children }: BoxProps) => {
 
   if (direction === 's') {
     resiseableProps = {
-      maxConstraints: [Infinity, window.innerHeight * 0.8],
+      maxConstraints: [Infinity, window.innerHeight * 0.7],
       height: window.innerHeight * 0.5,
       width: Infinity,
       resizeHandles: [direction],
